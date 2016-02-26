d4c06c635d9d6b0b1c49942895d5d8827801fad7
Jan Moxter <jan.moxter@innobix.com>
2016-02-26 15:33:15 -0500

    - Issue #4: The ESCP Template musty be able to retrieve data from the eFaps DataBase
    closes #4

--- src/main/efaps/ESJP/org/efaps/esjp/print/Template_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/print/Template_Base.java
@@ -16,10 +16,12 @@
  */
 package org.efaps.esjp.print;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.efaps.admin.event.Parameter;
 import org.efaps.admin.event.Return;
@@ -27,11 +29,23 @@ import org.efaps.admin.program.esjp.EFapsApplication;
 import org.efaps.admin.program.esjp.EFapsUUID;
 import org.efaps.db.Instance;
 import org.efaps.db.PrintQuery;
+import org.efaps.eql.InvokerUtil;
+import org.efaps.eql.stmt.IEQLStmt;
+import org.efaps.eql.stmt.parts.ISelectStmtPart;
+import org.efaps.esjp.ci.CIFormPrint;
 import org.efaps.esjp.ci.CIPrint;
+import org.efaps.esjp.common.AbstractCommon;
+import org.efaps.esjp.print.escp.DoubleHeightFunction;
+import org.efaps.esjp.print.escp.EFapsTemplate;
+import org.efaps.esjp.print.escp.EFapsTemplate.SubDataSource;
 import org.efaps.util.EFapsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import simple.escp.SimpleEscp;
-import simple.escp.json.JsonTemplate;
+import simple.escp.data.DataSources;
+import simple.escp.dom.Report;
+import simple.escp.fill.FillJob;
 
 /**
  * The Class Template_Base.
@@ -41,7 +55,82 @@ import simple.escp.json.JsonTemplate;
 @EFapsUUID("9816de5a-d96c-45c2-9ebc-4d914664dd6f")
 @EFapsApplication("eFapsApp-Print")
 public abstract class Template_Base
+    extends AbstractCommon
 {
+    /**
+     * Logger used in this class.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(Template.class);
+
+    /**
+     * Pattern for getting the Parameter including there special symbols.
+     */
+    private final Pattern parameterPattern = Pattern.compile("\\$P!\\{[a-z,A-Z,1-9,_,-]*\\}");
+
+    /**
+     * Pattern for getting the key of the Parameter.
+     */
+    private final Pattern keyPattern = Pattern.compile("(?<=\\{)([a-z,A-Z,1-9,_,-]*?)(?=\\})");
+
+    /**
+     * Replace parameters.
+     *
+     * @param _stmtStr stamentStr the parameters will be replace for
+     * @param _parameters the _parameters
+     * @return stmt
+     */
+    protected String replaceParameters(final String _stmtStr,
+                                       final Map<String, Object> _parameters)
+    {
+        final Pattern mainPattern = getParameterPattern();
+        final Pattern subPattern = getKeyPattern();
+        final Matcher matcher = mainPattern.matcher(_stmtStr);
+        final Map<String, String> replaceMap = new HashMap<>();
+        while (matcher.find()) {
+            final String mainStr = matcher.group();
+            final Matcher subMatcher = subPattern.matcher(mainStr);
+            subMatcher.find();
+            replaceMap.put(mainStr, getStringValue(_stmtStr, subMatcher.group(), _parameters));
+        }
+        String ret = _stmtStr;
+        for (final Entry<String, String> entry  :replaceMap.entrySet()) {
+            ret = ret.replace(entry.getKey(), entry.getValue());
+        }
+        LOG.debug("Stmt with replaced Parameters: {}", ret);
+        return ret;
+    }
+
+    /**
+     * Gets the string value.
+     *
+     * @param _stmtStr stamentStr the parameters will be replace for
+     * @param _key key the value will bee searched for
+     * @param _parameters the _parameters
+     * @return String representation of the object
+     */
+    protected String getStringValue(final String _stmtStr,
+                                    final String _key,
+                                    final Map<String, Object> _parameters)
+    {
+        String ret = "";
+        if (_key != null) {
+            final Object obj = _parameters.get(_key);
+            if (obj != null) {
+                LOG.trace("Found object to be replaced: {}", obj);
+                if (obj instanceof Instance) {
+                    if (_stmtStr.trim().startsWith("print")) {
+                        ret = ((Instance) obj).getOid();
+                    } else {
+                        ret = Long.valueOf(((Instance) obj).getId()).toString();
+                    }
+                } else {
+                    ret = obj.toString();
+                }
+            }
+        }
+        return ret;
+    }
+
 
     /**
      * Test print.
@@ -50,6 +139,7 @@ public abstract class Template_Base
      * @return the return
      * @throws EFapsException on error
      */
+    @SuppressWarnings("checkstyle:illegalcatch")
     public Return testPrint(final Parameter _parameter)
         throws EFapsException
     {
@@ -59,23 +149,81 @@ public abstract class Template_Base
             print.addAttribute(CIPrint.TemplateESCP.Template);
             print.execute();
             final String templStr = print.getAttribute(CIPrint.TemplateESCP.Template);
-            final JsonTemplate template = new JsonTemplate(templStr);
 
+            final EFapsTemplate template = new EFapsTemplate(templStr);
+            final Report report = template.parse();
+
+            final Map<String, Object> parameters = new HashMap<>();
             final Map<String, Object> value = new HashMap<>();
-            value.put("invoiceNo", "INVC-00001");
-            final List<Map<String, Object>> tables = new ArrayList<>();
-            for (int i=0; i<5; i++) {
-                final Map<String, Object> line = new HashMap<>();
-                line.put("code", String.format("CODE-%d", i));
-                line.put("name", String.format("Product Random %d", i));
-                line.put("qty", String.format("%d", i*i));
-                tables.add(line);
-            }
-            value.put("table_source", tables);
 
-            final SimpleEscp simpleEscp = new SimpleEscp();
-            simpleEscp.print(template, value);
+            final Instance objInst = Instance
+                            .get(_parameter.getParameterValue(CIFormPrint.Print_TemplatePrintTestForm.objectOID.name));
+            if (objInst.isValid()) {
+                parameters.put("INSTANCE", objInst);
+            }
+            try {
+                final String eql = replaceParameters(template.getEqlStmt(), parameters);
+                final IEQLStmt stmt = InvokerUtil.getInvoker().invoke(eql);
+                if (stmt instanceof ISelectStmtPart) {
+                    final List<Map<String, Object>> listmap = ((ISelectStmtPart) stmt).getData();
+                    if (listmap != null && !listmap.isEmpty()) {
+                        value.putAll(listmap.get(0));
+                    }
+                }
+            } catch (final Exception e) {
+                LOG.error("Catched error", e);
+            }
+            for (final SubDataSource sds  :template.getSubDataSources()) {
+                final String eql = replaceParameters(sds.getEqlStmt(), parameters);
+                try {
+                    final IEQLStmt stmt = InvokerUtil.getInvoker().invoke(eql);
+                    if (stmt instanceof ISelectStmtPart) {
+                        final List<Map<String, Object>> listmap = ((ISelectStmtPart) stmt).getData();
+                        if (sds.getPaging() > 0) {
+                            while (listmap.size() % sds.getPaging() != 0) {
+                                listmap.add(new HashMap<String, Object>());
+                            }
+                        }
+                        if (listmap != null && !listmap.isEmpty()) {
+                            value.put(sds.getKey(), listmap);
+                        }
+                    }
+                } catch (final Exception e) {
+                    LOG.error("Catched error", e);
+                }
+            }
+            FillJob.addFunction(DoubleHeightFunction.getFunction());
+
+            final FillJob fillJob = new FillJob(report, DataSources.from(value));
+
+            final boolean print2file = true;
+            if (print2file) {
+                LOG.info(fillJob.fill());
+            } else {
+                final SimpleEscp simpleEscp = new SimpleEscp();
+                simpleEscp.print(fillJob.fill());
+            }
         }
         return new Return();
+    }
+
+    /**
+     * Getter method for the instance variable {@link #pattern}.
+     *
+     * @return value of instance variable {@link #pattern}
+     */
+    protected Pattern getParameterPattern()
+    {
+        return this.parameterPattern;
+    }
+
+    /**
+     * Getter method for the instance variable {@link #keyPattern}.
+     *
+     * @return value of instance variable {@link #keyPattern}
+     */
+    protected Pattern getKeyPattern()
+    {
+        return this.keyPattern;
     }
 }
